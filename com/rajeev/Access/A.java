7241050ecfe21677edc93d43d6cd17e2315a7bf2
unknown <itrajeevranjan@gmail.com>
2024-02-11 01:08:41 +0530

    Oops

---
+++ com/rajeev/Access/A.java
@@ -0,0 +1,29 @@
+package com.rajeev.Access;
+
+import java.security.PrivateKey;
+
+public class A {
+    private int num;
+    private String name;
+    int[] arr;
+
+    //getter methods gets the value & setter methods sets the values of the
+    public int getNum() {
+        return num;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public A(String name , int num){
+        this.name = name;
+        this.num = num;
+        this.arr = new int[num];
+    }
+
+}
---
+++ com/rajeev/Access/Main.java
@@ -0,0 +1,8 @@
+package com.rajeev.Access;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A("Rajeev Ranjan" , 78);
+        System.out.println(obj.getNum());
+    }
+}
